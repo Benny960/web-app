22c25c4b1c25e1ed99cc2281a71094b8bab8d3bf
Ben O'Leary <49990800+Benny960@users.noreply.github.com>
2020-02-27 11:00:08 +0000

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,21 @@
+var logger = require("morgan"),
+cors = require("cors"),
+http = require("http"),
+express = require("express"),
+bodyParser = require("body-parser");
+
+var app = express();
+var port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/hello', function(req,res){res.json({result: 'Hello CCT'});
+                                   });
+app.get('/hello/:foo/:bar', function(req,res){res.json({result: 'hello Cct', data:[req.params.foo,req.params.bar]});
+                                             });
+
+app.post('/hello', function(req,res){res.json({result: 'The Post request was sent!', data: req.body});
+                                    });
+app.listen(port, function(err){console.log("listeningf on port:" + port);
+                              });
 
