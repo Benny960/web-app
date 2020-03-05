92765e24fc8d0b9e0750422f6a1d11e935acbce7
Ben O'Leary <49990800+Benny960@users.noreply.github.com>
2020-03-05 10:41:40 +0000

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
 var express = required('express'),
-router = express.router();
+router = express.router(),
+itemCtrl = require('./item-controller');
+
+router.get('/hello/:foo/:bar',itemCtrl.getWorld);
+router.post('/hello', );
+
 
 router.get('/hello/:foo/:bar', function(req,res){res.json({result: 'hello Cct', data:[req.params.foo,req.params.bar]});
                                              });
