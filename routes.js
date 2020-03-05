0b058f4358bc4f998d8fb23ef119f768734f6abd
Ben O'Leary <49990800+Benny960@users.noreply.github.com>
2020-03-05 10:20:56 +0000

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 var express = required('express'),
 router = express.router();
 
-router.get('/hello/:foo/:bar',function(req,res)){
-res.json({})
+router.get('/hello/:foo/:bar', function(req,res){res.json({result: 'hello Cct', data:[req.params.foo,req.params.bar]});
+                                             });
+module
 
 
 }
